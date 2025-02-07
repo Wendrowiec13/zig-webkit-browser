8ad22209a9aa3a2df032a9506dd280a09f9da016
Mateusz Kelner <mateuszkelner@gmail.com>
2025-02-07 19:17:54 +0100

    Create a build script linking AppKit, WebKit and objc

---
+++ build.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "zig-browser",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.linkLibC();
+    exe.linkFramework("WebKit");
+    exe.linkFramework("AppKit");
+    exe.linkSystemLibrary("objc");
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the browser");
+    run_step.dependOn(&run_cmd.step);
+}
